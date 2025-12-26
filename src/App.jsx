8dd21346b49b8b1724b596278dbe9acfdef28e07
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2025-12-26 12:10:53 -0300

    feat: add buttons in bag social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import BagSocialLinks from "./pages/BagSocialLinks"
 
-function App() {
+export default function App() {
   return (
     <div className="bg-gradient-01">
       <BagSocialLinks/> 
     </div>
   )
 }
-
-export default App
--- src/pages/BagSocialLinks/index.jsx
+++ src/pages/BagSocialLinks/index.jsx
@@ -1,10 +1,12 @@
-import Header from "../../components/header"
+import Buttons from "../../components/Buttons"
+import Header from "../../components/Header"
 
 export default function BagSocialLinks() {
   return (
-    <section className="sm:w-104 h-230 flex flex-col items-center justify-center">
-      <main>
-        <Header />
+    <section className="sm:w-104 h-230 flex flex-col items-center justify-center gap-16">
+      <Header />
+      <main className="sm:mt-9 flex flex-col items-center justify-center gap-6.5">
+        <Buttons />
       </main>
     </section>
   )
