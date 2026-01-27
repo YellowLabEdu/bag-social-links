d028ec95b6f0a97f360bd5650c0ff6cc60c0e403
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-01-26 21:48:33 -0300

    feat(modal): add shmusic modal

--- src/components/ModalsDark/ModalUnionAndPartnerships/index.jsx
+++ src/components/ModalsDark/ModalUnionAndPartnerships/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export function ModalLaFoca({ isOpen, onClose, children }) {
+export function Modal({ isOpen, onClose, children}) {
     if (!isOpen) return null;
 
     useEffect(() => {
@@ -14,8 +14,8 @@ export function ModalLaFoca({ isOpen, onClose, children }) {
         <div className="fixed inset-2 z-50 flex items-center justify-center">
             <div className="absolute inset-0 bg-black opacity-50 backdrop-blur-sm" onClick={onClose}></div>
             {/* Modal content */}
-            <div className="relative z-10 w-lg h-120 max-w-lg 
-            rounded-2xl bg-(--color-lafoca-dark) p-6 shadow-xl flex flex-col items-center justify-center gap-4">
+            <div className="relative z-10 w-lg h-128 max-w-lg 
+            rounded-2xl bg-(--color-blue-dark) p-10 shadow-xl flex flex-col items-center justify-center gap-4">
                 {children}
             </div>
         </div>
--- src/pages/BagSocialLinks/index.jsx
+++ src/pages/BagSocialLinks/index.jsx
@@ -3,11 +3,12 @@ import Header from "../../components/Header"
 import { ActionsButtons } from "../../components/ActionsButtons";
 import { Footer } from "../../components/Footer";
 import { ModalBag } from "../../components/ModalsDark/ModalBag";
-import { ModalLaFoca } from "../../components/ModalsDark/ModalLaFoca";
+import { Modal } from "../../components/ModalsDark/ModalUnionAndPartnerships";
 
 import BgYellowBagEdu from "../../assets/bg-yellowbagedu.png"
 import IconCircle from "../../assets/icon-circle.svg"
 import LaFoca from "../../assets/logo-lafoca.svg"
+import SHMusic from "../../assets/logo-shmusic.png"
 
 
 const actions = [
@@ -33,9 +34,30 @@ const actions = [
     }
 ];
 
+const unionsAndPartnerships = [
+    {
+        id: 0,
+        name: 'LAFoca',
+        img: LaFoca,
+        description: 'O Laboratório de Abordagens de Ensino Focadas no Aluno (LAFoca) foi fundado em 2017. O grupo é formado por alunos e professores, além de parceiros de outras instituições de ensino.',
+        link: 'https://www.instagram.com/lafocameta/'            
+      },
+      {
+        id:   1,
+        name: 'SHMusic',
+        img: SHMusic,
+        description: 'A SHMusic oferece cursos de música para crianças, jovens e adultos, com aulas dinâmicas e acessíveis que estimulam criatividade, desenvolvimento técnico e paixão pela arte.',
+        link: 'https://www.instagram.com/shmusic.escola2/', 
+      },
+];
+
 export default function BagSocialLinks() {
   const [open01, setOpen01] = useState(false);
   const [open02, setOpen02] = useState(false);
+
+  const partnershipsArray = Object.values(unionsAndPartnerships);
+  const [currentPartnershipIndex, setCurrentPartnershipIndex] = useState(0);
+  const partnership = partnershipsArray[currentPartnershipIndex];
 
   return (
     <div className="bg-gradient-01 w-98 h-230 flex flex-col justify-center items-center gap-6 m-auto">
@@ -85,46 +107,51 @@ export default function BagSocialLinks() {
               onClick={() => setOpen01(false)}
               > SAIR </button>
           </ModalBag>
-          <ModalLaFoca
+          <Modal
             isOpen={open02}
             onClose={() => setOpen02(false)}
-          >
-            <div className="bg-(--color-white) w-84 h-98 rounded-2xl flex flex-col items-center justify-start gap-2">
+          >    
+            <div className="bg-(--color-white) w-84 h-90 rounded-2xl flex flex-col gap-2 p-6">
               <button
-                className="self-end cursor-pointer"
-                onClick={() => setOpen02(false)}
-              >
-                 <img src={IconCircle} alt="ícone para sair do Modal" />
+                  className="self-end cursor-pointer"
+                  onClick={() => setOpen02(false)}
+                >
+                <img src={IconCircle} alt="ícone para sair do Modal" />
               </button>
-              <div className=" w-80 flex flex-col items-center gap-8 p-4">
-                <img src={LaFoca} alt="" />
-                <span>@LAFoca</span>
-                <p className="text-(--color-color-dark-gray) text-sm text-justify">
-                  O Laboratório de Abordagens de Ensino Focadas no Aluno (LAFoca) foi fundado em 2017. 
-                  O grupo é formado por alunos e professores, além de parceiros de 
-                  outras instituições de ensino.  
-                  <br /> <br />
-                </p>
-              </div>
-              <button
-              className="w-52 h-12 bg-(--color-tech-blue-light) rounded-2xl 
-              text-(--color-white) font-semibold hover:scale-95 transition-transform duration-200 cursor-pointer"
-              onClick={() => window.open('https://www.instagram.com/lafocameta/', '_blank', 'noopener,noreferrer')}
-              > SAIBA MAIS </button>
+              <div key={partnership.id} className="flex flex-col items-center gap-4 px-4 md:px-6">
+                  <img src={partnership.img} alt={`${partnership.name} logo`} />
+                  <span>@{partnership.name}</span>
+                  <p className="text-(--color-color-dark-gray) text-sm text-center pb-2">
+                    {partnership.description}
+                  </p>
+                  <button
+                  className="w-52 h-12 bg-(--color-tech-blue-light) rounded-2xl 
+                  text-(--color-white) font-semibold hover:scale-95 transition-transform duration-200 cursor-pointer"
+                  onClick={() => window.open(partnership.link, '_blank', 'noopener,noreferrer')}
+                  > SAIBA MAIS </button>
+                </div>
             </div>
-            <div className="flex flex-row-reverse items-center justify-center gap-4">
+            <div className="flex flex-row-reverse items-center justify-center gap-2">              
               <button
               className="w-40 h-12 bg-emerald-500 rounded-2xl 
               text-(--color-white) font-semibold hover:scale-95 transition-transform duration-200 cursor-pointer"
-              onClick={() => setOpen02(false)}
-              > PRÓXIMO </button>
+                onClick={() =>
+                   setCurrentPartnershipIndex((prev) => prev + 1 < partnershipsArray.length ? prev + 1 : 0
+                  )}> PRÓXIMO </button>
               <button
-                className="w-40 h-12 bg-red-500 rounded-2xl 
+                className="w-40 h-12 bg-blue-500 rounded-2xl 
                 text-(--color-white) font-semibold hover:scale-95 transition-transform duration-200 cursor-pointer"
-                onClick={() => setOpen02(false)}
-                > SAIR </button>
+                onClick={() =>
+                   setCurrentPartnershipIndex((prev) => prev - 1 >= 0 ? prev - 1 : partnershipsArray.length - 1
+                  )}
+                > ANTERIOR </button>
             </div>
-          </ModalLaFoca>  
+            <button
+              className="w-40 h-12 bg-red-500 rounded-2xl
+              text-(--color-white) font-semibold hover:scale-95 transition-transform duration-200 cursor-pointer"
+              onClick={() => setOpen02(false)}
+              > SAIR </button>
+          </Modal>  
         </main>    
       </div>  
       <Footer />  
