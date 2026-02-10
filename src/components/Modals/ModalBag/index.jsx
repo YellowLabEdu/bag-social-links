b2e63845fae4a4c44eec4b43d23cc7304daf022f
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-02-10 19:57:29 -0300

    fix(layout): add fix of modal layout

--- src/components/Modals/ModalBag/index.jsx
+++ src/components/Modals/ModalBag/index.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 
 export function ModalBag({ isOpen, onClose, children, color }) {
+    const isMobileOrTablet = window.matchMedia("(max-width: 1024px)").matches;
     useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = "hidden";
+      isMobileOrTablet ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";
     }
-
     return () => {
       document.body.style.overflow = "auto";
     };
@@ -14,10 +14,10 @@ export function ModalBag({ isOpen, onClose, children, color }) {
   if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 z-50 flex items-center justify-center px-4">
             <div className="absolute inset-0 bg-black opacity-50 backdrop-blur-sm" onClick={onClose}></div>
             {/* Modal content */}
-            <div className={`relative z-10 w-98 h-112 max-w-sm sm:max-w-md lg:max-w-lg 
+            <div className={`relative z-10 w-94 max-w-sm sm:max-w-md lg:max-w-lg max-h-176 
             rounded-2xl ${color} p-10 shadow-xl flex flex-col items-center justify-center gap-4`}>
                 {children}
             </div>
