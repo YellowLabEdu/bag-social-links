358e65194c19ebc025f35223a239659a570e0ae7
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-02-16 21:05:38 -0300

    fix(header): add adjusts in layout of header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import baglogo from "../../assets/bag-logo.svg";
+import toggleImg from "../../assets/Switch.svg";
 
-export default function Header({color}) {
+
+export default function Header({color, mode}) {
+  const [Mode, setMode] = useState(true);
+
   return (
-    <header className="flex flex-col justify-center items-center gap-4">
-      <img className="w-32 h-32" src={baglogo} alt="Logo da Bag"/>
-      <span className={` w-45 h-8 text-2xl ${color} font-Nunito 
-      font-semibold`}>
-        @yellowbagedu
-      </span>
+    <header className="flex flex-col justify-center items-center gap-16">
+      <div className="flex flex-col justify-center items-center">
+        <img className="w-32 h-32" src={baglogo} alt="Logo da Bag"/>
+        <span className={` w-45 h-8 text-2xl ${color} font-Nunito 
+        font-semibold`}>
+          @yellowbagedu
+        </span>
+      </div>
+      <button className="flex items-center justify-center active:scale-95 transition rotate-y-0"  
+      aria-label="Alternar Tema">
+        <img
+          src={toggleImg}
+          alt="Toggle de Temas"
+          className="w-17 h-12"/>
+      </button>
     </header>
   );
 }
--- src/pages/BagSocialLinks/index.jsx
+++ src/pages/BagSocialLinks/index.jsx
@@ -10,7 +10,6 @@ import BgYellowBagEdu02 from "../../assets/bg-ybedu-02.svg"
 import IconCircle from "../../assets/icon-circle.svg"
 import LaFoca from "../../assets/logo-lafoca.svg"
 import SHMusic from "../../assets/logo-shmusic.svg"
-import toggleImg from "../../assets/Switch.svg";
 
 
 const actions = [
@@ -39,26 +38,12 @@ const actions = [
 export default function BagSocialLinks() {
   const [open01, setOpen01] = useState(false);
   const [open02, setOpen02] = useState(false);
-  const [Mode, setMode] = useState(true);
 
   return (
-      <div className={Mode ? "bg-gradient-02 min-h-230 md:min-h-screen flex items-center justify-center" : "bg-gradient-01 min-h-230 md:min-h-screen flex items-center justify-center"}  >
+      <div className={"bg-gradient-02 min-h-230 md:min-h-screen flex items-center justify-center"}  >
         <img className="hidden lg:block" src={BgYellowBagEdu01} alt="Background YellowBagEdu" />
-         <div className="flex flex-col items-center justify-center gap-8 ">
-          <Header 
-          color={Mode ? "text-(--color-color-dark-gray)" : "text-(--color-white)"}
-          />
-          <button className={Mode ? "flex items-center justify-center active:scale-95 transition rotate-y-0" : "flex items-center justify-center active:scale-95 transition rotate-y-180"} aria-label="Alternar Tema"
-          onClick={() => {
-            console.log("clicou");
-            setMode(!Mode);
-          }}
-          >
-            <img
-              src={toggleImg}
-              alt="Toggle de Temas"
-              className="w-17 h-12"/>
-          </button>
+         <div className="flex flex-col items-center justify-center">
+          <Header />
           <main className="flex flex-col items-center justify-center gap-6">
             {actions.map((action) => {
               return (
@@ -68,17 +53,14 @@ export default function BagSocialLinks() {
                     href={action.href}
                     label={action.label}
                     onClick={() => action.id === 1 ? setOpen01(true) : setOpen02(true)}
-                    color={Mode ? "bg-(--color-tech-blue-light) text-(--color-white)" : "bg-(--color-color-yellow) text-(--color-color-dark-gray)"}
                   />
               );
             })} 
           </main>   
-        <Footer
-          color={Mode ? "bg-(--color-tech-blue-light)" : "bg-(--color-color-yellow)"}
-        />  
+        <Footer />  
       </div>
       <img className="hidden lg:block" src={BgYellowBagEdu02} alt="Background YellowBagEdu" />  
-      {open01 && <ModalBag isOpen={open01} onClose={() => setOpen01(false)} color={Mode ?  "bg-(--color-color-yellow)" : "bg-(--color-blue-dark)"}>         
+      {open01 && <ModalBag isOpen={open01} onClose={() => setOpen01(false)}>         
     <div className="bg-white w-84 h-88 rounded-2xl flex flex-col items-center justify-center gap-6">
       <button
         className="self-end cursor-pointer"
@@ -101,7 +83,7 @@ export default function BagSocialLinks() {
       </div>
     </div>
     </ModalBag>}
-    { open02 && <Modal isOpen={open02} onClose={() => setOpen02(false)} color={Mode ?  "bg-(--color-color-yellow)" : "bg-(--color-blue-dark)"}>
+    { open02 && <Modal isOpen={open02} onClose={() => setOpen02(false)}>
       <div className="bg-white w-80 h-58 rounded-2xl flex flex-col gap-6">
           <button
               className="self-end cursor-pointer"
